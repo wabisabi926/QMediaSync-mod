50853b17ccfaae4d60758ba2e8749acf63830166
dean <qicfan@gmail.com>
2026-03-17 22:48:53 +0800

    去掉欢迎横幅

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -100,8 +100,6 @@
             </template>
           </el-dropdown>
         </div>
-
-        <Banner />
         <router-view />
       </el-main>
     </el-container>
@@ -175,7 +173,6 @@ import { ref, onMounted, onUnmounted, computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 import { useBackupStore } from '@/stores/backup'
-import Banner from '@/components/Banner.vue'
 import { ElMessage, ElMessageBox } from 'element-plus'
 import { isMobile as checkIsMobile, onDeviceTypeChange } from '@/utils/deviceUtils'
 import { formatDuration } from '@/utils/timeUtils'
